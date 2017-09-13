39d5a074bdd3e252e0166660828b98f7eed1b5cd
cshelt99 <31927310+cshelt99@users.noreply.github.com>
2017-09-13 12:55:58 -0400

    Add files via upload
    Got total to work.

--- guess.js
+++ guess.js
@@ -5,16 +5,34 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function playGame(player) {
+function start() {
+    var playAgain = 'y';
+	
+    do {
+        playGame();
+        playAgain = prompt("Would you like to play again? (y or n)");
+    }
+    while(playAgain == 'y');
+}
+
+alert("Steel your mind for this, the ultimate game of skill!");
+var total = 0;
+start();
+
+function playGame() {
     var Turns = 0;
     var Correct = 0;
-    var Answer = getRandomInt(1,100);
+    var Answer = getRandomInt(1,1);
+	
     do {
         var Guess = prompt("Guess the integer between 0 and 100!");
         Turns++;
         if (isNaN(Guess) !== true) {
             if (Guess == Answer) {
+				total = total + Turns;
+				alert(total);
                 alert("CORRECT! You guessed it in "+Turns+" turns.");
+				alert("You had a total of "+total+ "guesses");
                 Correct = 1;
             }
             else if (Guess > Answer) {
@@ -26,20 +44,11 @@ function playGame(player) {
         }
         else {
             alert("Quitter!");
-            player.losses ++;
+            quit
         }
     }
     while (Correct == 0);
 }
 
-function start() {
-    var playAgain = 'y';
-    do {
-        playGame();
-        playAgain = prompt("Would you like to play again? (y or n)");
-    }
-    while(playAgain == 'y');
-}
 
-alert("Steel your mind for this, the ultimate game of skill!");
-start();
+
